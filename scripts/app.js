783129d9b42d18bb561acccb2d95dc383670a5d8
Heya Na <naheya1126@gmail.com>
2016-05-11 22:31:03 -0400

    Added service for main controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
-angular.module('ToDoList', []);
+angular.module('ToDoList', [])
+.controller('mainCtrl', function($scope, dataService) {
+	$scope.helloConsole = dataService.helloConsole;
+	$scope.learningNgChange = function(){
+		console.log("Hello there!");
+	};
+
+	$scope.todos = [
+		{"name" : "clean the house"},
+		{"name" : "run"}
+	]
+})
+
+.service('dataService', function(){
+	this.helloConsole = function(){
+		console.log('Hello console');
+	}
+});
