70206a20c3640b070d68b9494a201d93885f9165
Heya Na <naheya1126@gmail.com>
2016-05-13 11:04:55 -0400

    Get json file from the server

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,23 @@
 angular.module('ToDoList', [])
-.controller('mainCtrl', function($scope) {
+
+.controller('mainCtrl', function($scope, dataService) {
 	$scope.helloWorld = function(){
 		console.log("Hello there!");
 	};
-
-	$scope.todos = [
-		{"name" : "clean the house"},
-		{"name" : "run"}
-	]
+	$scope.helloConsole = dataService.helloConsole;
+	dataService.getTodos(function(response){
+		console.log(response.data);
+		$scope.todos = response.data;
+	});
 })
 
-.service('dataService', function(){
+.service('dataService', function($http){
 	this.helloConsole = function(){
 		console.log('Hello console');
-	}
+	};
+
+	this.getTodos = function(callback){
+		$http.get('mock/todos.json')
+		.then(callback);
+	};
 });
