9f2b6549f9276cdcbeb07d203da576d327507ec4
Heya Na <naheya1126@gmail.com>
2016-05-15 10:30:56 -0400

    Added style to completed tasked and move them to the bottom

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,1 @@
 angular.module('ToDoList', [])
-.service('dataService', function($http){
-	this.helloConsole = function(){
-		console.log('Hello console');
-	};
-
-	this.getTodos = function(callback){
-		$http.get('mock/todos.json')
-		.then(callback);
-	};
-
-	this.deleteTodo = function(todo) {
-		console.log("The" + todo.name + " has been deleted.");
-	};
-
-	this.saveTodo = function(todo) {
-		console.log("The " + todo.name + " has been saved");
-	}
-});
